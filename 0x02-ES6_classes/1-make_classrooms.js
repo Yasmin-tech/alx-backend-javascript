95c4115ba8ac6500771089bc83e6631857168868
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-29 00:45:13 +0300

    add task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,19 @@
+/**
+ * Implement a function that return an array of 3 classroom objects
+ *
+ */
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  /**
+   * a function that returns an array of 3 ClassRoom objects with the sizes
+   * 19, 20, and 34 (in this order).
+   *
+   */
+  const arrayOfClassRoomObjs = [];
+  arrayOfClassRoomObjs.push(new ClassRoom(19));
+  arrayOfClassRoomObjs.push(new ClassRoom(20));
+  arrayOfClassRoomObjs.push(new ClassRoom(34));
+
+  return arrayOfClassRoomObjs;
+}
