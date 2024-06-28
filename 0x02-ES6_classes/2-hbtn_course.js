e7745c6635521871edfa6b598762652477fae471
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-06-29 01:22:03 +0300

    add task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+/**
+ * Represents a Holberton Course.
+ */
+
+export default class HolbertonCourse {
+   /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  set length(l) {
+    if (typeof l !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = l;
+  }
+
+  set students(studentsArray) {
+    if (!(Array.isArray(studentsArray))) {
+      throw new TypeError('Students must be an Array');
+    }
+    this._students = studentsArray;
+  }
+}
