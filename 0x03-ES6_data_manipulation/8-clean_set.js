7781e1a526a6718d2e28b8fbb5f68a0437cdeffe
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-03 03:25:30 +0300

    add task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,25 @@
+/**
+ * This moudel contains a function that checks if a set os strings start
+ * with the given string
+ * @return {String} a new string contains the rest of the string from the set
+ *  that satisfy the condition
+ */
+
+export default function cleanSet(theSet, text) {
+  let newString = '';
+  let i = 0;
+
+  if (!text) {
+    return newString;
+  }
+  for (const str of theSet) {
+    if (str.startsWith(text)) {
+      if (i !== 0) {
+        newString += '-';
+      }
+      newString += str.slice(text.length);
+      i += 1;
+    }
+  }
+  return newString;
+}
