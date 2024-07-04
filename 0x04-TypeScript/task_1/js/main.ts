8344ffb8f6a45caca916b54174344a0c362e8f96
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 03:03:01 +0300

    add task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,37 @@ function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
 
+interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass
+}
+
+interface IStudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework() :string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string{
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+    
+}
+
 // Test examples
 
 const teacher3: Teacher = {
