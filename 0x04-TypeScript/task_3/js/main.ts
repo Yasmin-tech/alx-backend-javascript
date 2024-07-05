61f50c7288464cf70eeeb620ce9d3a336676f1f3
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-05 19:19:42 +0300

    add task 8

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+/// <reference types="node" />
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud"
+
+let row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+}
+
+const newRowID: RowID = CRUD.insertRow(row)
+const updatedRow: RowElement = {...row, age: 23}
+CRUD.updateRow(newRowID, updatedRow)
+CRUD.deleteRow(newRowID)
