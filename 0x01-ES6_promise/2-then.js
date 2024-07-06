f9656246057850708f343baad5e0b53b8ebd03f4
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-07 00:42:15 +0300

    add task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+// A function that append 3 handlers to the promise when it resolves,
+// rejected and a final message
+
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
