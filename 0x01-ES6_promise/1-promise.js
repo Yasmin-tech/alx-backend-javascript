d0997e2e0bf31287b16c74dd9d0e3f1704eddf7b
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-06 15:32:19 +0300

    fix the returned object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,8 +4,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-        firstName: 'Guillaume',
-        lastName: 'Salva',
+        status: 200,
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
