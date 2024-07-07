d029f76531271ab7524366fbb232a254b99fa0a7
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-08 01:47:08 +0300

    add task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,13 @@
-import signUpUser from './4-user-promise.js';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
-    .then((responses) => responses.map((res) => ({
-      status: res.status,
-      value: res.status === 'fulfilled' ? res.value : String(res.reason),
-    })));
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
