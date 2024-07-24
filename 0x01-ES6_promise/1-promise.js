fbd058a6936d0a43bed1abcc155e6bf3630c380a
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-24 16:21:53 +0300

    add task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+/*
+ * Return a promise that resolve to an object if success
+ * and reject otherwise
+ */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
