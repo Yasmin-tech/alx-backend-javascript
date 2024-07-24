f71a65d37677452bca42aa9361553efeb70240f9
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-24 17:05:43 +0300

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,9 +5,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([{ body }, { firstName, lastName }]) => {
-      console.log(body, firstName, lastName);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
