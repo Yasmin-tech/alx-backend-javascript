56f21d720bdf9c7892dba3bd68732fddc19ae157
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-25 00:23:57 +0300

    add task 7

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,8 @@
+/*
+ *  A function that accepts 2 promises and returns
+ *  the value returned by the promise that resolved the first
+ */
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.any([chinaDownload, USDownload]);
+}
