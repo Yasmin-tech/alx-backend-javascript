6633f64c34373ff4aab4c385804fb5ec6738ec67
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-25 22:08:01 +0300

    add the readme file

--- 0x02-ES6_classes/0-classroom.js
+++
@@ -1,8 +0,0 @@
-// This module contains the implementation of the class named ClassRoom
-// It accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
-
-export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
-}
--- 0x02-ES6_classes/README.md
+++ 0x02-ES6_classes/README.md
@@ -4,7 +4,7 @@
 
 At the end of this project, we are expected to be able to  [explain to anyone] **without the help of Google**:
 
-- How to define a Class
+- How to define Classes
 - How to add methods to a class
 - Why and how to add a static method to a class
 - How to extend a class from another
