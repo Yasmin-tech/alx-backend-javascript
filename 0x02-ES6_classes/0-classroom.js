e1570a2ae8ad5e5b447bb53e8b069e64b24b3c31
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-25 22:12:19 +0300

    add task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// This module contains the implementation of the class named ClassRoom
+// It accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
