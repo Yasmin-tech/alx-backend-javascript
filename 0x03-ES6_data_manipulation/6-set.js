c8c3a662996d66a545efa2cdb4c7b29196f9cbae
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-29 23:14:27 +0300

    add task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * A function that accepts an argument (Array, of any kind of element,
+ * and return a set from the array
+ */
+
+export default function setFromArray(theArray) {
+  return new Set(theArray);
+}
