635e1a778ca02602bf4468acb19e12c6bc94e054
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-29 23:29:40 +0300

    add task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,7 +2,7 @@
  * This modules contains hasValuesFromArray function that
  * returns a boolean if all the elements in the array exist within the set
  */
+
 export default function hasValuesFromArray(theSet, theArray) {
-  const theArrayAsSet = new Set(theArray);
-  return theArrayAsSet.isSubsetOf(theSet);
+  return theArray.every((element) => theSet.has(element));
 }
