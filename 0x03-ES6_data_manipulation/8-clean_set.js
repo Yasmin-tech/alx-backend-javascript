d2c57d6be6fd4f85f8e127906565f579891f59c2
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-30 04:46:56 +0300

    add task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,23 @@
+/**
+ * This script contains a function that checks if a set of strings start
+ * with the given string
+ * @return {String} a new string contains the rest of the string from the set
+ *  that satisfy the condition
+ */
+
+export default function cleanSet(theSet, startString) {
+  if (!theSet || !(theSet instanceof Set)
+    || !startString || typeof startString !== 'string') {
+    return '';
+  }
+  const listOfSetStrings = [];
+  for (const value of theSet) {
+    if (typeof value !== 'string') {
+      return '';
+    }
+    if (value.startsWith(startString)) {
+      listOfSetStrings.push(value.slice(startString.length));
+    }
+  }
+  return listOfSetStrings.join('-');
+}
