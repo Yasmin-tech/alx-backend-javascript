bcfcb96d331abd4a957b47101cd7aed864c530f2
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-10-01 09:26:04 +0300

    fix the code 3

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,33 @@
-// This program should be executed through a command line
-// User should enter their name after the prompt in a new line
+// // This program should be executed through a command line
+// // User should enter their name after the prompt in a new line
 
-const process = require('node:process');
+// const process = require('node:process');
+
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// // Read the name from STDIN
+// process.stdin.on('readable', () => {
+//   const name = process.stdin.read();
+//   if (name !== null) {
+//     process.stdout.write(`Your name is: ${name}`);
+//   }
+// });
+
+// // Write a message before closing
+
+// process.stdin.on('end', () => {
+//   process.stdout.write('This important software is now closing\n');
+// });
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Read the name from STDIN
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
-
-// Write a message before closing
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
